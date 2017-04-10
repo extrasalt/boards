c2c47359d77487720332432e02d548bd67387ca6
Mohanarangan Muthukumar <mohan@extrasalt.org>
2017-04-10 20:21:54 +0530

    Updated desc of the root command

--- cmd/root.go
+++ cmd/root.go
@@ -27,16 +27,9 @@ var cfgFile string
 // RootCmd represents the base command when called without any subcommands
 var RootCmd = &cobra.Command{
 	Use:   "boards",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-// Uncomment the following line if your bare application
-// has an action associated with it:
-//	Run: func(cmd *cobra.Command, args []string) { },
+	Short: "boards is a personal productivity tool",
+	Long: `boards is a personal productivity tool that 
+lets you manage your todos and view them in columns`,
 }
 
 // Execute adds all child commands to the root command sets flags appropriately.
@@ -68,8 +61,8 @@ func initConfig() {
 	}
 
 	viper.SetConfigName(".boards") // name of config file (without extension)
-	viper.AddConfigPath("$HOME")  // adding home directory as first search path
-	viper.AutomaticEnv()          // read in environment variables that match
+	viper.AddConfigPath("$HOME")   // adding home directory as first search path
+	viper.AutomaticEnv()           // read in environment variables that match
 
 	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
